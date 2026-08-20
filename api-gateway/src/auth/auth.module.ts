8312e0e53378a0355f3effed1a33488aa6b308a2
joschonarth <joschonarth@gmail.com>
2026-08-20 20:04:53 -0300

    feat(api-gateway): add AuthController with login and registration endpoints

--- api-gateway/src/auth/auth.module.ts
+++ api-gateway/src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './service/auth.service';
+import { AuthController } from './controllers/auth.controller';
 
 @Module({
   imports: [
@@ -20,5 +21,6 @@ import { AuthService } from './service/auth.service';
   ],
   exports: [AuthService],
   providers: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}

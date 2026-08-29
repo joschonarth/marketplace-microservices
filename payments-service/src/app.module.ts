c65e88f8a2c6a28e302776803ae17c02ca48b6e6
joschonarth <joschonarth@gmail.com>
2026-08-29 08:01:50 -0300

    feat(payments): set up database configuration and initial service structure

--- payments-service/src/app.module.ts
+++ payments-service/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { databaseConfig } from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(databaseConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
--- payments-service/src/main.ts
+++ payments-service/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const port = process.env.PORT || 3004;
+  await app.listen(port);
+
+  console.log(`💳 Payments Service running on port ${port}`);
 }
 bootstrap();

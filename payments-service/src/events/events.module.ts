5353a95e080e89f02601b545523a1908f1f3d5af
joschonarth <joschonarth@gmail.com>
2026-08-30 08:30:12 -0300

    feat(payments): add DLQ controller for message management

--- payments-service/src/events/events.module.ts
+++ payments-service/src/events/events.module.ts
@@ -4,9 +4,11 @@ import { ConfigModule } from '@nestjs/config';
 import { PaymentQueueService } from './payment-queue/payment-queue.service';
 import { PaymentConsumerService } from './payment-consumer/payment-consumer.service';
 import { DlqService } from './dlq/dlq.service';
+import { DlqController } from './dlq/dlq.controller';
 
 @Module({
   imports: [ConfigModule],
+  controllers: [DlqController],
   providers: [
     RabbitmqService,
     PaymentQueueService,
